b8e28bdf5d08c2e57f3bef12a5e718a741bc348c
Anerti <e.andrianombana@protonmail.com>
2025-12-18 11:23:15 +0300

    feat: listing method not implemented

--- src/main/java/org/example/DataRetriever.java
+++ src/main/java/org/example/DataRetriever.java
@@ -1,5 +1,31 @@
 package org.example;
 
+import java.util.List;
+
 public class DataRetriever {
-    
+    private final DBConnection connection =  new DBConnection();
+
+    public Dish findDishById(Integer id){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<Ingredients> findIngredients(int page, int size){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<Ingredients> createIngredients(List<Ingredients> newIngredients){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public Dish saveDish(Dish dishToSave){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<Dish> findDishsByIngredientName(String IngredientName){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<Ingredients> findIngredientsByCriteria(String ingredientName, CategoryEnum category, String dishName, int page, int size){
+        throw  new UnsupportedOperationException("Not supported yet.");
+    }
 }
